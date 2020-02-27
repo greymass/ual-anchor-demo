8aab83f6045f8f1bf8734b8102d675db9aff4b00
Aaron Cox <aaron@greymass.com>
2020-02-27 18:56:11 -0500

    Reworked demo to support multiple testnets and ual-anchor 0.2.0

--- src/App.js
+++ src/App.js
@@ -4,15 +4,85 @@ import PropTypes from 'prop-types'
 import { Anchor } from 'ual-anchor'
 import { UALProvider, withUAL } from 'ual-reactjs-renderer'
 
-const receiver = process.env.REACT_APP_TO
-const getTransaction = (account) => ({
-  actions: [{
-    account: 'eosio.token',
-    name: 'transfer',
-    authorization: [{ actor: account, permission: 'active' }],
-    data: { from: account, to: receiver, quantity: '0.0001 EOS', memo: '' },
-  }],
-})
+const chains = [{
+  chainId: 'e70aaab8997e1dfce58fbfac80cbbb8fecec7b99cf982a9444273cbc64c41473',
+  name: 'Jungle 2 (Testnet)',
+  rpcEndpoints: [{
+    protocol: 'https',
+    host: 'jungle.greymass.com',
+    port: 443,
+  }]
+},{
+  chainId: '2a02a0053e5a8cf73a56ba0fda11e4d92e0238a4a2aa74fccf46d5a910746840',
+  name: 'Jungle 3 (Testnet)',
+  rpcEndpoints: [{
+    protocol: 'https',
+    host: 'jungle3.greymass.com',
+    port: 443,
+  }]
+},{
+  chainId: 'f11d5128e07177823924a07df63bf59fbd07e52c44bc77d16acc1c6e9d22d37b',
+  name: 'Lynx (Testnet)',
+  rpcEndpoints: [{
+    protocol: 'https',
+    host: 'lynxtestnet.greymass.com',
+    port: 443,
+  }]
+},{
+  chainId: '1eaa0824707c8c16bd25145493bf062aecddfeb56c736f6ba6397f3195f33c9f',
+  name: 'Telos (Testnet)',
+  rpcEndpoints: [{
+    protocol: 'https',
+    host: 'testnet.eos.miami',
+    port: 443,
+  }]
+},{
+  chainId: 'f16b1833c747c43682f4386fca9cbb327929334a762755ebec17f6f23c9b8a12',
+  name: 'WAX (Testnet)',
+  rpcEndpoints: [{
+    protocol: 'https',
+    host: 'waxtestnet.greymass.com',
+    port: 443,
+  }]
+}];
+
+const getTransaction = (account, chainId) => {
+  return {
+    actions: [{
+      account: 'eosio.token',
+      name: 'transfer',
+      authorization: [{ actor: account, permission: 'active' }],
+      data: {
+        from: account,
+        to: 'teamgreymass',
+        quantity: getTransactionAmount(chainId),
+        memo: 'ual-anchor-demo'
+      },
+    }],
+  }
+}
+
+const getTransactionAmount = (chainId) => {
+  let symbol = 'EOS'
+  let quantity = '0.0001'
+  switch (chainId) {
+    case 'f11d5128e07177823924a07df63bf59fbd07e52c44bc77d16acc1c6e9d22d37b': {
+      symbol = 'LNX'
+      quantity = '0.00000001'
+      break
+    }
+    case '1eaa0824707c8c16bd25145493bf062aecddfeb56c736f6ba6397f3195f33c9f': {
+      symbol = 'TLOS'
+      break
+    }
+    case 'f16b1833c747c43682f4386fca9cbb327929334a762755ebec17f6f23c9b8a12': {
+      symbol = 'WAX'
+      quantity = '0.00000001'
+      break
+    }
+  }
+  return `${quantity} ${symbol}`
+}
 
 class TestApp extends Component {
   static propTypes = {
@@ -24,15 +94,23 @@ class TestApp extends Component {
     }).isRequired,
   }
 
-  state = { message: '' }
+  constructor(props) {
+    super(props)
+    this.state = {
+      chainId: props.chainId,
+      message: '',
+    }
+  }
 
-  transfer = async () => {
+  purchase = async () => {
     const { ual: { activeUser } } = this.props
     try {
       const accountName = await activeUser.getAccountName()
-      const demoTransaction = getTransaction(accountName)
-      const result = await activeUser.signTransaction(demoTransaction, { expireSeconds: 60, blocksBehind: 3 })
-      this.setState({ message: `Transfer Successful!` }, () => {
+      const demoTransaction = getTransaction(accountName, await activeUser.getChainId())
+      const result = await activeUser.signTransaction(demoTransaction, { expireSeconds: 120, blocksBehind: 3 })
+      this.setState({
+        message: `Transfer Successful!`,
+      }, () => {
         setTimeout(this.resetMessage, 5000)
       })
       console.info('SUCCESS:', result)
@@ -52,15 +130,25 @@ class TestApp extends Component {
           </div>
         )
       }
-      {(this.props.ual.activeUser)
-        ? (
-          <p>{this.props.ual.activeUser.accountName}@{this.props.ual.activeUser.requestPermission}</p>
-        )
-        : false
-      }
-
-      <button type='button' onClick={this.transfer} style={{ ...styles.button, ...styles.blueBG }}>
-        <p style={styles.baseText}>{`Transfer 0.0001 EOS`}</p>
+      <table>
+        <tbody>
+          <tr>
+            <td style={styles.td}>Blockchain</td>
+            <td style={styles.td}>{this.props.ual.chains[0].name}</td>
+          </tr>
+          <tr>
+            <td style={styles.td}>Account</td>
+            <td style={styles.td}>{this.props.ual.activeUser.accountName}</td>
+          </tr>
+          <tr>
+            <td style={styles.td}>Permission</td>
+            <td style={styles.td}>{this.props.ual.activeUser.requestPermission}</td>
+          </tr>
+        </tbody>
+      </table>
+      <p>Actions:</p>
+      <button type='button' onClick={this.purchase} style={{ ...styles.button, ...styles.blueBG }}>
+        <p style={styles.baseText}>{`Transfer 0.0001`}</p>
       </button>
       <button type='button' onClick={this.props.ual.logout} style={styles.logout}>
         <p>Logout</p>
@@ -70,19 +158,50 @@ class TestApp extends Component {
 
   renderLoginButton = () => (
     <button type='button' onClick={this.props.ual.showModal} style={styles.button}>
-      <p style={{buttonText: styles.buttonText, baseText: styles.baseText}}>LOGIN w/ Jungle Account</p>
+      <p style={{buttonText: styles.buttonText, baseText: styles.baseText}}>Login using {this.props.ual.chains[0].name}</p>
     </button>
   )
 
   render() {
-    const {
-      ual: {
-        activeUser,
-      }
-    } = this.props
+    const { ual: { activeUser } } = this.props
+    return (activeUser) ? this.renderLoggedInView() : this.renderLoginButton()
+  }
+}
+
+class UALWrapper extends Component {
+  state = { chainId: chains[0].chainId }
+  setChain = (event) => this.setState({ chainId: event.target.value })
+  render () {
+    const { chainId } = this.state
+    const { available } = this.props
+    const [ chain ] = available.filter((c) => c.chainId === chainId)
+    const anchor = new Anchor([chain], { appName: 'ual-anchor-demo' })
     return (
       <div style={styles.container}>
-        {activeUser ? this.renderLoggedInView() : this.renderLoginButton()}
+        <p>
+          <select
+            defaultValue={chainId}
+            onChange={this.setChain}
+            style={styles.select}
+          >
+            {chains.map((chain) => (
+              <option
+                key={chain.chainId}
+                value={chain.chainId}
+              >
+                {chain.name}
+              </option>
+            ))}
+          </select>
+        </p>
+        <UALProvider
+          appName='Anchor + Authenticator Test App'
+          authenticators={[anchor]}
+          chains={[chain]}
+          key={chain.chainId}
+        >
+          <TestAppConsumer />
+        </UALProvider>
       </div>
     )
   }
@@ -106,6 +225,13 @@ const styles = {
     fontSize: 18,
     fontWeight: 'bold',
   },
+  select: {
+    textAlign: 'center',
+    borderRadius: 5,
+    fontSize: 24,
+    fontWeight: 'bold',
+    width: '200px',
+  },
   logout: {
     marginTop: 20,
   },
@@ -127,22 +253,10 @@ const styles = {
   },
 }
 
-const chainId = process.env.REACT_APP_CHAIN_ID
-const rpcEndpoints = [{
-  protocol: process.env.REACT_APP_RPC_PROTOCOL,
-  host: process.env.REACT_APP_RPC_HOST,
-  port: process.env.REACT_APP_RPC_PORT,
-}]
-const exampleNet = { chainId, rpcEndpoints }
 const TestAppConsumer = withUAL(TestApp)
-const anchor = new Anchor([exampleNet], {
-  appName: 'ual-anchor-demogi'
-})
 
 const App = () => (
-  <UALProvider chains={[exampleNet]} authenticators={[anchor]} appName='Anchor + Authenticator Test App'>
-    <TestAppConsumer />
-  </UALProvider>
+  <UALWrapper available={chains} />
 )
 
 export default App
